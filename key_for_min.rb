6214fc5f53434410c162274e51e41ec43a4ef418
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 03:10:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
-def key_for_min_value(ikea)
+key_for_min_value(ikea)
   lowest = " "
   ikea.each do | furniture, price |
     if price < 30
