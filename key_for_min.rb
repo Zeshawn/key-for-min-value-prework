22680a402de361a09e19a1cd9360010c03b67b53
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 03:12:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-ikea = {:chair => 25, :table => 85, :mattress => 450}
 
-def key_for_min_value(ikea)
-  if ikea.length == 0
+
+def key_for_min_value(name_hash)
+  if name_hash.length == 0
     return nil
+    
 end
